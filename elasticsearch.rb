9e7ce72a3d2183800ebc135be920a095f8d8dff9
Asa Calow <mothership@asacalow.co.uk>
2013-05-30 23:53:52 +0100

    Remote elasticsearch server ahoy! NB: should move this out into a config file somewhere.

--- elasticsearch.rb
+++ elasticsearch.rb
@@ -1,3 +1,3 @@
 require './elasticsearch/collection_item'
 
-# Tire.configure { url "http://USERNAME:PASSWORD@ec2-54-216-92-54.eu-west-1.compute.amazonaws.com:8080/" }
+Tire.configure { url "http://USERNAME:PASSWORD@elasticsearch.asacalow.me:8080/" }
